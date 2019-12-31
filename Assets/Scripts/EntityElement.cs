e7ea50556c64fd3f187bceed91bafc76f06216d1
superflouz <superflouz@hotmail.com>
2019-12-31 21:57:25 +0100

    Add blob and elements enttities

---
+++ Assets/Scripts/EntityElement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityElement : MonoBehaviour
+{
+	private Gameplay.Element element;
+	public Gameplay.Element Element { 
+		get {
+			return element;
+		}
+		set {
+			element = value;
+
+			if (animator != null) {
+				switch (value) {
+					case Gameplay.Element.Fire:
+						animator.SetTrigger("Fire");
+						break;
+					case Gameplay.Element.Earth:
+						animator.SetTrigger("Earth");
+						break;
+					case Gameplay.Element.Water:
+						animator.SetTrigger("Water");
+						break;
+				}
+			}
+		}
+	}
+
+	private Animator animator;
+
+	private void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+
+    }
+
+}

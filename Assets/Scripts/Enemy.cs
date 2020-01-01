ab01b382d5cf8393c45c2fea7e8c694b3d443003
superflouz <superflouz@hotmail.com>
2020-01-01 13:35:35 +0100

    Add wave generator

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+	EntityElement entityElement;
+
 	int index = 0;
 	List<Vector3> path;
+	Vector3 offset;
 
-    // Start is called before the first frame update
-    void Start()
+	public float speed;
+
+	private void Awake()
+	{
+		entityElement = GetComponent<EntityElement>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
+		offset = new Vector3(Random.Range(-0.3f, 0.3f), Random.Range(-0.3f, 0.3f), 0);
+		transform.Translate(offset);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		if (transform.position != path[index] + offset) {
+			transform.position = Vector3.MoveTowards(transform.position, path[index] + offset, speed * Time.deltaTime);
+			if (transform.position == path[index] + offset) {
+				index++;
+				if (index == path.Count) {
+					index = 0;
+				}
+			}
+		}
     }
+
+	public void SetElement(Gameplay.Element elem)
+	{
+		entityElement.Element = elem;
+	}
+
+	public void SetPath(List<Vector3> selectedPath)
+	{
+		path = selectedPath;
+	}
 
 	public void ApplyDamages(int damages, Gameplay.Element element)
 	{
--- Assets/Scripts/WavesManager.cs
+++ Assets/Scripts/WavesManager.cs
@@ -7,11 +7,47 @@ public class WavesManager : MonoBehaviour
 	public List<Vector3> Path1;
 	public List<Vector3> Path2;
 
+	public Enemy Blob;
+
+	public int waveLevel;
+	public Gameplay.Element element;
+	public int additionnalFire;
+	public int additionnalEarth;
+	public int additionnalWater;
+
 	// Start is called before the first frame update
 	void Start()
     {
-        
-    }
+		GenerateWave();
+
+	}
+
+	void GenerateWave()
+	{
+		for (int i = 0; i < (waveLevel / 2) + 4; i++) {
+			Enemy enemy = Instantiate(Blob, new Vector3(-20, 7, 0) + Vector3.left * Random.Range(0, 0.5f * waveLevel), Quaternion.identity);
+			enemy.SetPath(Path1);
+			enemy.SetElement(element);
+		}
+
+		for (int i = 0; i < additionnalFire * ((waveLevel / 10) + 1); i++) {
+			Enemy enemy = Instantiate(Blob, new Vector3(-20, 7, 0) + Vector3.left * Random.Range(0, 0.5f * waveLevel), Quaternion.identity);
+			enemy.SetPath(Path1);
+			enemy.SetElement(Gameplay.Element.Fire);
+		}
+
+		for (int i = 0; i < additionnalEarth * ((waveLevel / 10) + 1); i++) {
+			Enemy enemy = Instantiate(Blob, new Vector3(-20, 7, 0) + Vector3.left * Random.Range(0, 0.5f * waveLevel), Quaternion.identity);
+			enemy.SetPath(Path1);
+			enemy.SetElement(Gameplay.Element.Earth);
+		}
+
+		for (int i = 0; i < additionnalWater * ((waveLevel / 10) + 1); i++) {
+			Enemy enemy = Instantiate(Blob, new Vector3(-20, 7, 0) + Vector3.left * Random.Range(0, 0.5f * waveLevel), Quaternion.identity);
+			enemy.SetPath(Path1);
+			enemy.SetElement(Gameplay.Element.Water);
+		}
+	}
 
     // Update is called once per frame
     void Update()
